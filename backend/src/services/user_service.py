9f0b0276e48af0ab2d3797a80dd55df853a5f2b7
HPSTokki <sorachisenpai@gmail.com>
2026-02-22 16:04:19 +0800

    Hashing Password Done, Register User Updated

--- backend/src/services/user_service.py
+++ backend/src/services/user_service.py
@@ -1,5 +1,6 @@
 from sqlmodel import Session, select
 
+from src.core.security import hash_password
 from src.dto.todolist_dto import InsertUser, ListReadUser
 from src.models.todolist_model import User, Task
 
@@ -17,6 +18,10 @@ class UserService():
         
     def register_user(self, user_data: InsertUser) -> User:
         user = User.model_validate(user_data)
+        
+        hashed_password = hash_password(user_data.password)
+        
+        user.password = hashed_password
         
         self.session.add(user)
         self.session.commit()
