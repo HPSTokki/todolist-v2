ddf7baee8327e7c348e197b8ff9d5a65e2705b8b
HPSTokki <sorachisenpai@gmail.com>
2026-02-22 16:30:25 +0800

    Restart Again

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,6 @@
-import uvicorn
+def main():
+    print("Hello from backend!")
+
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "src.main:app",
-        port=8000,
-        log_level=True,
-        reload=True
-    )
+    main()
--- backend/src/core/security.py
+++
@@ -1,9 +0,0 @@
-from pwdlib import PasswordHash
-
-pwd_context = PasswordHash.recommended()
-
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
-def verify_password(password: str, hash_password: str) -> bool:
-    return pwd_context.verify(password, hash_password)
